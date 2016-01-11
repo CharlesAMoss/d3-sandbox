7c2ef870e818ba5ebd26c20b0fac31dac4f4a43a
CharlesAMoss <charlesamoss@yahoo.com>
2016-01-10 19:32:48 -0800

    add axis

--- js/main.js
+++ js/main.js
@@ -43,9 +43,12 @@ var padding = 50;
 
 var viz = d3.select("#viz-wrapper")
                 .append('svg')
+                .attr('height', height + (padding * 2))
+                .attr('width', width + (padding * 2))
+                .append('g')
                 .attr('id', 'viz')
-                .attr('height', height)
-                .attr('width', width);
+                .attr('transform',
+                    'translate(' + padding + ',' + padding + ')');
 
 var yScale = d3.scale.linear()
                         .range([height, 0 ]);
@@ -53,15 +56,31 @@ var yScale = d3.scale.linear()
 var xScale = d3.time.scale()
                         .range([0, width]);
 
+var xAxis = d3.svg.axis().scale(xScale)
+                        .orient("bottom")
+                        .ticks(20);
+
+var yAxis = d3.svg.axis().scale(yScale)
+                        .orient("left")
+                        .ticks(8);
+
 var parseTime = d3.time.format("%Y%m%d");
 
 d3.csv('climate_data.csv', function(data) {
     yDomain = d3.extent(data, function(element) {
-        return parseInt(element.TMAX);
+        return parseInt(element.TMAX) * 1.1;
     });
 
-    yDomain = d3.extent(data, function(element) {
-        return parseInt(element.TMAX);
+    xMin = d3.min(data, function(element) {
+       time = parseTime.parse(element.DATE);
+       time.setMonth(time.getMonth() - 1);
+       return time;
+    });
+
+    xMax = d3.max(data, function(element) {
+       time = parseTime.parse(element.DATE);
+       time.setMonth(time.getMonth() + 1);
+       return time;
     });
 
     xDomain = d3.extent(data, function(element) {
@@ -69,31 +88,65 @@ d3.csv('climate_data.csv', function(data) {
     });
 
     yScale.domain(yDomain);
-    xScale.domain(xDomain);
+    xScale.domain([xMin, xMax]);
 
-    dots = viz.selectAll('circle')
-                .data(data)
-                .enter()
-                .append('circle');
+    viz.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll('text')
+        .attr("transform", function() {
+               return "rotate(-65)";
+               })
+        .style("text-anchor", "end")
+        .style('font-size', '10px')
+        .attr("dx", "-10px")
+        .attr("dy", "10px");
 
-    dots.attr('r', function(d, i) {
-            return Math.abs(d.TMAX) / 8; })
-        .attr('cx', function(d) {
+    viz.append("g")
+         .attr("class", "y axis")
+         .call(yAxis);
+
+    dots = viz.selectAll('g.dots')
+            .data(data)
+            .enter()
+            .append('g')
+            .attr('class', 'dots');
+
+    dots.attr('transform', function(d) {
+            // get the x position
             date = parseTime.parse(d.DATE);
-            return xScale(date); })
-        .attr('cy', function(d) {
-            return yScale(d.TMAX); })
-        .style('stroke', '#f1f1f1')
-        .style('stroke-width', '2px')
-        .style('fill', function(d) {
-            year = d.DATE.charAt(3);
-            return year === '3' ? '#b3b3b3' : '#d9d9d9';
-        })
-        .style('stroke', function(d) {
-            year = d.DATE.charAt(3);
-            return year === '3' ? '#f1f1f1' : '#a3a3a3';
+            x = xScale(date);
+            // get the y position
+            y = yScale(d.TMAX);
+            return 'translate(' + x + ',' + y + ')';
         });
+        //.style('stroke-width', '1px')
+        // .style('fill', function(d) {
+        //     year = d.DATE.charAt(3);
+        //     return year === '3' ? '#b3b3b3' : '#d9d9d9';
+        // })
+        // .style('stroke', function(d) {
+        //     year = d.DATE.charAt(3);
+        //     return year === '3' ? '#d9d9d9' : '#a3a3a3';
+        // });
 
+    dots.append('circle')
+        .attr('r', 5)
+        .style('fill','#d9d9d9');
+        //.attr('r', function(d, i) {
+        //    return Math.abs(d.TMAX) / 12; });
+        // .attr('cx', function(d) {
+        //     date = parseTime.parse(d.DATE);
+        //     return xScale(date); })
+        // .attr('cy', function(d) {
+        //     return yScale(d.TMAX); });
+
+    dots.append('text')
+        .style('fill','#000')
+        .text(function(d) {
+            return d.TMAX;
+    });
 
     // dots.attr('r', function(d, i) {
     //     return Math.abs(d.TMAX) / 10; })
